2eb2b10b0a7db4ba128486f748eae84f614ba453
= <=>
2021-03-03 12:01:41 -0300

    ajustes

--- src/main/java/com/javatechie/reg/service/api/config/InternacionalizacaoConfig.java
+++ src/main/java/com/javatechie/reg/service/api/config/InternacionalizacaoConfig.java
@@ -3,6 +3,7 @@ package com.javatechie.reg.service.api.config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
@@ -18,14 +19,14 @@ public class InternacionalizacaoConfig {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
         messageSource.setBasename("classpath:messages");
         messageSource.setDefaultEncoding("ISO-8859-1");
-        //messageSource.setDefaultLocale(Locale.getDefault());
         return messageSource;
     }
+
 
     @Bean
     public LocaleResolver localeResolver() {
         SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.getDefault());
+        slr.setDefaultLocale(new Locale("pt", "BR"));
         return slr;
     }
 
